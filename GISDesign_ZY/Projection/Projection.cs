cda56e6c4d74e6ca8d11425159672d55b47a5af8
miemieyanga <1700012404@pku.edu.cn>
2020-05-31 19:22:29 +0800

    modified:   GISDesign_ZY/MapControl.cs 	modified:   GISDesign_ZY/Projection/Projection.cs

--- GISDesign_ZY/Projection/Projection.cs
+++ GISDesign_ZY/Projection/Projection.cs
@@ -16,6 +16,7 @@ namespace GISDesign_ZY
         abstract public PointF XYToLngLat(PointF coordinate);
         abstract public void LngLatToXY(MapManager map);
         abstract public void LngLatToXY(Layer layer);
+        abstract public void XYToLnglat(Layer layer);
     }
 
     /// <summary>
@@ -28,6 +29,43 @@ namespace GISDesign_ZY
         const double e2 = (a * a - b * b) / (a * a);
         const double center = 110;
         static double e = Math.Sqrt(e2);
+
+
+        public override void XYToLnglat(Layer layer)
+        {
+            for (int i = 0; i < layer.MRecords.records.Count(); i++)
+            {
+                object feature = layer.MRecords.records.Item(i).Value(1);
+
+                switch (layer.FeatureType)
+                {
+                    case FeatureTypeConstant.PointD:
+                        PointD cp = XYToLngLat((PointD)feature);
+                        feature = cp;
+                        break;
+                    case FeatureTypeConstant.Polyline:
+                        Polyline polyline = (Polyline)feature;
+                        PointD[] points = new PointD[polyline.points.Length];
+                        for (int j = 0; j < points.Length; j++)
+                        {
+                            points[j] = XYToLngLat(polyline.points[j]);
+                        }
+                        polyline.points = points;
+                        feature = polyline;
+                        break;
+                    case FeatureTypeConstant.Polygon:
+                        Polygon polygon = (Polygon)feature;
+                        PointD[] points1 = new PointD[polygon.points.Length];
+                        for (int j = 0; j < points1.Length; j++)
+                        {
+                            points1[j] = XYToLngLat(polygon.points[j]);
+                        }
+                        polygon.points = points1;
+                        feature = polygon;
+                        break;
+                }
+            }
+        }
 
         public override void LngLatToXY(MapManager map)
         {
